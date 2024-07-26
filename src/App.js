efb6db240c33a8e0319c6919d098b1ae82c3f92b
SallYBot <mr.sharkyn@gmail.com>
2024-07-26 16:39:50 -0700

    feat: migrate to GIS google sign in

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import "@material/card/dist/mdc.card.css";
 import "./App.css";
 
 class App extends Component {
+
+  // =================== Default react component methods ===================
+
   constructor() {
     super();
 
@@ -35,30 +38,84 @@ class App extends Component {
   }
 
   componentDidMount() {
-    window.gapi.load("client:auth2", () => {
-      window.gapi.client
-        .init({
-          discoveryDocs: [
-            "https://sheets.googleapis.com/$discovery/rest?version=v4"
-          ],
-          clientId: this.clientId,
-          scope:
-            "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly",
-          plugin_name:
-            "shark-expenses-web"
-        })
-        .then(() => {
-          window.gapi.auth2
-            .getAuthInstance()
-            .isSignedIn.listen(this.signedInChanged);
-          this.signedInChanged(
-            window.gapi.auth2.getAuthInstance().isSignedIn.get()
-          );
-        })
-        .catch(err => console.error('Error initializing Google API client:', err));
-    });
+    this.loadGisClient();
     document.addEventListener("keyup", this.onKeyPressed.bind(this));
   }
+
+  render() {
+    return (
+        <div>
+          <header className="mdc-toolbar mdc-toolbar--fixed">
+            <div className="mdc-toolbar__row">
+              <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
+                <span className="mdc-toolbar__title">Expenses</span>
+              </section>
+              <section
+                  className="mdc-toolbar__section mdc-toolbar__section--align-end"
+                  role="toolbar"
+              >
+                {this.state.signedIn === false &&
+                    <a
+                        className="material-icons mdc-toolbar__icon"
+                        aria-label="Sign in"
+                        alt="Sign in"
+                        onClick={this.signIn}
+                    >
+                      perm_identity
+                    </a>}
+                {this.state.signedIn &&
+                    <a
+                        className="material-icons mdc-toolbar__icon"
+                        aria-label="Sign out"
+                        alt="Sign out"
+                        onClick={this.signOut}
+                    >
+                      exit_to_app
+                    </a>}
+              </section>
+            </div>
+          </header>
+          <div className="toolbar-adjusted-content">
+            {this.state.signedIn === undefined && <LoadingBar/>}
+            {this.state.signedIn === false &&
+                <div className="center">
+                  <button
+                      className="mdc-button sign-in"
+                      aria-label="Sign in"
+                      onClick={() => {
+                        this.signIn();
+                      }}
+                  >
+                    Sign In
+                  </button>
+                </div>}
+            {this.state.signedIn && this.renderBody()}
+          </div>
+          <div
+              ref={el => {
+                if (el) {
+                  this.snackbar = new MDCSnackbar(el);
+                }
+              }}
+              className="mdc-snackbar"
+              aria-live="assertive"
+              aria-atomic="true"
+              aria-hidden="true"
+          >
+            <div className="mdc-snackbar__text"/>
+            <div className="mdc-snackbar__action-wrapper">
+              <button
+                  type="button"
+                  className="mdc-button mdc-snackbar__action-button"
+                  aria-hidden="true"
+              />
+            </div>
+          </div>
+        </div>
+    );
+  }
+
+  // =================== Business logic ===================
 
   onKeyPressed = (e) => {
     if (this.state.signedIn === true) {
@@ -71,13 +128,6 @@ class App extends Component {
           this.handleExpenseCancel()
         }
       }
-    }
-  }
-
-  signedInChanged = (signedIn) => {
-    this.setState({ signedIn: signedIn });
-    if (this.state.signedIn) {
-      this.load();
     }
   }
 
@@ -244,135 +294,138 @@ class App extends Component {
       });
   }
 
-  render() {
-    return (
-      <div>
-        <header className="mdc-toolbar mdc-toolbar--fixed">
-          <div className="mdc-toolbar__row">
-            <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
-              <span className="mdc-toolbar__title">Expenses</span>
-            </section>
-            <section
-              className="mdc-toolbar__section mdc-toolbar__section--align-end"
-              role="toolbar"
-            >
-              {this.state.signedIn === false &&
-                <a
-                  className="material-icons mdc-toolbar__icon"
-                  aria-label="Sign in"
-                  alt="Sign in"
-                  onClick={e => {
-                    e.preventDefault();
-                    window.gapi.auth2.getAuthInstance().signIn();
-                  }}
-                >
-                  perm_identity
-                </a>}
-              {this.state.signedIn &&
-                <a
-                  className="material-icons mdc-toolbar__icon"
-                  aria-label="Sign out"
-                  alt="Sign out"
-                  onClick={e => {
-                    e.preventDefault();
-                    window.gapi.auth2.getAuthInstance().signOut();
-                  }}
-                >
-                  exit_to_app
-                </a>}
-            </section>
-          </div>
-        </header>
-        <div className="toolbar-adjusted-content">
-          {this.state.signedIn === undefined && <LoadingBar />}
-          {this.state.signedIn === false &&
-            <div className="center">
-              <button
-                className="mdc-button sign-in"
-                aria-label="Sign in"
-                onClick={() => {
-                  window.gapi.auth2.getAuthInstance().signIn();
-                }}
-              >
-                Sign In
-              </button>
-            </div>}
-          {this.state.signedIn && this.renderBody()}
-        </div>
-        <div
-          ref={el => {
-            if (el) {
-              this.snackbar = new MDCSnackbar(el);
-            }
-          }}
-          className="mdc-snackbar"
-          aria-live="assertive"
-          aria-atomic="true"
-          aria-hidden="true"
-        >
-          <div className="mdc-snackbar__text" />
-          <div className="mdc-snackbar__action-wrapper">
-            <button
-              type="button"
-              className="mdc-button mdc-snackbar__action-button"
-              aria-hidden="true"
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+
 
   renderBody() {
-    if (this.state.processing) return <LoadingBar />;
+    if (this.state.processing) return <LoadingBar/>;
     else
       return (
-        <div className="content">
-          {this.renderExpenses()}
-        </div>
+          <div className="content">
+            {this.renderExpenses()}
+          </div>
       );
   }
 
   renderExpenses() {
     if (this.state.showExpenseForm)
       return (
-        <ExpenseForm
-          categories={this.state.categories}
-          accounts={this.state.accounts}
-          expense={this.state.expense}
-          onSubmit={this.handleExpenseSubmit}
-          onCancel={this.handleExpenseCancel}
-          onDelete={this.handleExpenseDelete}
-          onChange={this.handleExpenseChange}
-        />
+          <ExpenseForm
+              categories={this.state.categories}
+              accounts={this.state.accounts}
+              expense={this.state.expense}
+              onSubmit={this.handleExpenseSubmit}
+              onCancel={this.handleExpenseCancel}
+              onDelete={this.handleExpenseDelete}
+              onChange={this.handleExpenseChange}
+          />
       );
     else
       return (
-        <div>
-          <div className="mdc-card">
-            <section className="mdc-card__primary">
-              <h2 className="mdc-card__subtitle">This month you've spent:</h2>
-              <h1 className="mdc-card__title mdc-card__title--large center">
-                {this.state.currentMonth}
-              </h1>
-            </section>
-            <section className="mdc-card__supporting-text">
-              Previous month: {this.state.previousMonth}
-            </section>
-          </div>
-          <ExpenseList
-            expenses={this.state.expenses}
-            onSelect={this.handleExpenseSelect}
-          />
-          <button
-            onClick={() => this.onExpenseNew()}
-            className="mdc-fab app-fab--absolute material-icons"
-            aria-label="Add expense"
-          >
-            <span className="mdc-fab__icon">add</span>
-          </button>
+          <div>
+            <div className="mdc-card">
+              <section className="mdc-card__primary">
+                <h2 className="mdc-card__subtitle">This month you've spent:</h2>
+                <h1 className="mdc-card__title mdc-card__title--large center">
+                  {this.state.currentMonth}
+                </h1>
+              </section>
+              <section className="mdc-card__supporting-text">
+            Previous month: {this.state.previousMonth}
+          </section>
         </div>
-      );
+        <ExpenseList
+          expenses={this.state.expenses}
+          onSelect={this.handleExpenseSelect}
+        />
+        <button
+          onClick={() => this.onExpenseNew()}
+          className="mdc-fab app-fab--absolute material-icons"
+          aria-label="Add expense"
+        >
+          <span className="mdc-fab__icon">add</span>
+        </button>
+      </div>
+    );
+}
+
+// =================== Google auth/api clients ===================
+
+  loadGisClient = () => {
+    const gisScript = document.createElement("script");
+    gisScript.src = "https://accounts.google.com/gsi/client";
+    gisScript.async = true;
+    gisScript.defer = true;
+    gisScript.onload = this.loadGapiClient;
+    document.body.appendChild(gisScript);
+  }
+
+  loadGapiClient = () => {
+    const gapiScript = document.createElement("script");
+    gapiScript.src = "https://apis.google.com/js/api.js";
+    gapiScript.async = true;
+    gapiScript.defer = true;
+    gapiScript.onload = () => {
+      window.gapi.load('client', this.initializeClients);
+    };
+    document.body.appendChild(gapiScript);
+  }
+
+  initializeClients = () => {
+    this.initializeGisClient();
+    this.initializeGapiClient();
+  }
+
+  initializeGisClient = () => {
+    if (window.google && window.google.accounts && window.google.accounts.oauth2) {
+      this.tokenClient = window.google.accounts.oauth2.initTokenClient({
+        client_id: this.clientId,
+        scope: "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly",
+        callback: this.handleCredentialResponse,
+      });
+      this.checkSignInStatus();
+    } else {
+      console.error('Google Identity Services failed to load');
+    }
+  }
+
+  handleCredentialResponse = (response) => {
+    if (response && response.access_token) {
+      window.gapi.client.setToken({access_token: response.access_token});
+      this.setState({ signedIn: true });
+      this.load();
+    }
+  }
+
+  checkSignInStatus = () => {
+    const token = window.gapi.client.getToken();
+    if (token && token.access_token) {
+      this.setState({ signedIn: true });
+      this.load();
+    } else {
+      this.setState({ signedIn: false });
+    }
+  }
+
+  signIn = () => {
+    this.tokenClient.requestAccessToken({ prompt: 'consent' });
+  }
+
+  signOut = () => {
+    const token = window.gapi.client.getToken();
+    if (token !== null) {
+      window.google.accounts.oauth2.revoke(token.access_token, () => {
+        window.gapi.client.setToken('');
+        this.setState({ signedIn: false });
+      });
+    }
+  }
+
+  initializeGapiClient = () => {
+    window.gapi.client.init({
+      discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    }).then(() => {
+      this.checkSignInStatus();
+    }).catch(err => console.error('Error initializing Google API client:', err));
   }
 }
 
